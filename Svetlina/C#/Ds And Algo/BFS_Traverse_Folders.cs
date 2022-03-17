45338065ab3ceff0fc21c20a4e38bb3c77a3519e
Superalex-dev <mr.naidobrixjr@gmail.com>
2022-03-17 21:44:59 +0200

    Initial commit

---
+++ Svetlina/C#/Ds And Algo/BFS_Traverse_Folders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Trees_and_Graphs
+{
+    class BFS_Traverse_Folders
+    {
+        public static void Main(string[] args)
+        {
+            TraverseDirBFS(@"C:\Windows\assembly");
+        }
+        private static void TraverseDirBFS(DirectoryInfo dir, string spaces)
+        {
+            Console.WriteLine(spaces + dir.FullName);
+
+            DirectoryInfo[] children = dir.GetDirectories();
+            foreach (DirectoryInfo child in children)
+            {
+                TraverseDirBFS(child, spaces + " ");
+            }
+        }
+
+        public static void TraverseDirBFS(string directoryPath)
+        {
+            TraverseDirBFS(new DirectoryInfo(directoryPath), string.Empty);
+        }
+    }
+}
---
+++ Svetlina/C#/Ds And Algo/DFS_Traverse_Folders_and_Files.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Trees_and_Graphs
+{
+    class DFS_Traverse_Folders_and_Files
+    {
+        public static void Main(string[] args)
+        {
+            TraverseDirBFS(@"C:\Windows\assembly");
+        }
+        private static void TraverseDirDFS(DirectoryInfo dir, string spaces)
+        {
+            Console.WriteLine(spaces + dir.FullName);
+
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(spaces + " " + file.FullName);
+            }
+
+            DirectoryInfo[] children = dir.GetDirectories();
+
+            foreach (DirectoryInfo child in children)
+            {
+                TraverseDirDFS(child, spaces + " ");
+            }
+        }
+
+        public static void TraverseDirBFS(string directoryPath)
+        {
+            TraverseDirDFS(new DirectoryInfo(directoryPath), string.Empty);
+        }
+    }
+}
