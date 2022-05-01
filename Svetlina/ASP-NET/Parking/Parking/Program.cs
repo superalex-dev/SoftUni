895c6e40f951fc862a37fe851b2005efc8894fa1
superalex-dev <mr.naidobrixjr@gmail.com>
2022-05-01 13:50:23 +0300

    Initial commit

--- Svetlina/ASP-NET/Parking/Parking/Program.cs
+++ Svetlina/ASP-NET/Parking/Parking/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<Parking.Models.ParkingContext>();
+//builder.Services.AddDbContext<Parking.Models.ParkingContext>();
 
 var app = builder.Build();
 
