7f3aa03bf4229ea063e825ef20ff9358f7de0c10
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-05-05 12:10:41 +0300

    Initial commit

--- Svetlina/C#/Ds And Algo/Sorting Algorithms/Sorting algo/Inversion count/Program.cs
+++ Svetlina/C#/Ds And Algo/Sorting Algorithms/Sorting algo/Inversion count/Program.cs
@@ -62,10 +62,7 @@ namespace Inversion_count
         }
         static void Main(string[] args)
         {
-            int[] arr = Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse)
-                .ToArray();
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             Console.Write(mergeSort(arr, 5));
         }
     }
