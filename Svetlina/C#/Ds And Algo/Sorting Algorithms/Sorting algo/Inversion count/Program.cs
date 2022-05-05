f7f1a15f5db66885a653c492fe09686898badeeb
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-05-05 12:18:46 +0300

    Update

--- Svetlina/C#/Ds And Algo/Sorting Algorithms/Sorting algo/Inversion count/Program.cs
+++ Svetlina/C#/Ds And Algo/Sorting Algorithms/Sorting algo/Inversion count/Program.cs
@@ -5,65 +5,77 @@ namespace Inversion_count
 {
     public class Program
     {
-        static int mergeSort(int[] arr, int array_size)
+        static void Main(string[] args)
         {
-            int[] temp = new int[array_size];
-            return _mergeSort(arr, temp, 0, array_size - 1);
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(MergeSort(arr));
         }
-        static int _mergeSort(int[] arr, int[] temp, int left, int right)
+        private static int MergeSort(int[] arr)
         {
-            int mid, inv_count = 0;
+            int[] temp = new int[arr.Length];
+            return Sort(arr, temp, 0, arr.Length - 1);
+        }
+        private static int Sort(int[] arr, int[] temp, int left, int right)
+        {
+            int mid, invCount = 0;
+
             if (right > left)
             {
                 mid = (right + left) / 2;
-                inv_count += _mergeSort(arr, temp, left, mid);
-                inv_count += _mergeSort(arr, temp, mid + 1, right);
 
-                inv_count += merge(arr, temp, left, mid + 1, right);
+                invCount = Sort(arr, temp, left, mid);
+                invCount += Sort(arr, temp, mid + 1, right);
+
+                invCount += Merge(arr, temp, left, mid + 1, right);
             }
-            return inv_count;
+
+            return invCount;
         }
-        static int merge(int[] arr, int[] temp, int left, int mid, int right)
+        private static int Merge(int[] arr, int[] temp, int left, int mid, int right)
         {
             int i, j, k;
-            int inv_count = 0;
+
+            int invCount = 0;
 
             i = left;
             j = mid;
             k = left;
+
             while ((i <= mid - 1) && (j <= right))
             {
                 if (arr[i] <= arr[j])
                 {
                     temp[k++] = arr[i++];
                 }
+
                 else
                 {
                     temp[k++] = arr[j++];
-                    inv_count = inv_count + (mid - i);
+                    invCount += mid - i;
                 }
             }
+
             while (i <= mid - 1)
             {
                 temp[k++] = arr[i++];
             }
+
 
             while (j <= right)
             {
                 temp[k++] = arr[j++];
             }
 
+
             for (i = left; i <= right; i++)
             {
                 arr[i] = temp[i];
             }
 
-            return inv_count;
-        }
-        static void Main(string[] args)
-        {
-            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            Console.Write(mergeSort(arr, 5));
+            return invCount;
         }
     }
 }
