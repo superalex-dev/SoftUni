0db456abc4b53a08a709f0b7164781c90cc72126
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-05-12 13:09:16 +0300

    Update

--- Svetlina/C#/Ds And Algo/Searching Algorithms/SearchingAlgo/SearchingAlgo/Program.cs
+++ Svetlina/C#/Ds And Algo/Searching Algorithms/SearchingAlgo/SearchingAlgo/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
 
 namespace SearchingAlgo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int search = int.Parse(Console.ReadLine());
+
+            var index = BinarySearch(arr, search);
+            Console.WriteLine(index);
+        }
+
+        public static int BinarySearch(int[] arr, int key)
+        {
+            int leftIndex = 0;
+            int rightIndex = arr.Length - 1;
+            
+
+            while (leftIndex < rightIndex)
+            {
+                int midIndex = (leftIndex + rightIndex) / 2;
+
+                if (key < arr[midIndex])
+                {
+                    rightIndex = midIndex - 1;
+                }
+                else if (key > arr[midIndex])
+                {
+                    leftIndex = midIndex + 1;
+                }
+
+                else
+                {
+                    return midIndex;
+                }
+            }
+
+            
+
+            return -1;
         }
     }
 }
