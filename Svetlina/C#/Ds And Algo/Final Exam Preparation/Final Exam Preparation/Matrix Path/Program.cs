d62ac29ea726ac76e207f20183e762fb03b5bd63
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-05-19 13:41:03 +0300

    Initial commit

--- Svetlina/C#/Ds And Algo/Final Exam Preparation/Final Exam Preparation/Matrix Path/Program.cs
+++ Svetlina/C#/Ds And Algo/Final Exam Preparation/Final Exam Preparation/Matrix Path/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Matrix_Path
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
--- Svetlina/C#/Ds And Algo/Final Exam Preparation/Final Exam Preparation/Word Generator/Program.cs
+++ Svetlina/C#/Ds And Algo/Final Exam Preparation/Final Exam Preparation/Word Generator/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Linq;
 
 namespace Word_Generator
 {
     public class Program
     {
+        static char[] arr;
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+
+            arr = new char[n];
+
+            WordGenerator(n, 0);
+        }
+
+        private static void WordGenerator(int n, int current)
+        {
+            if (current >= n)
+            {
+                Console.WriteLine(String.Join("", arr));
+                return;
+            }
+
+            for (int i = 97; i < 97 + n; i++)
+            {
+                arr[current] = (char)i;
+                WordGenerator(n, current + 1);
+            }
         }
     }
 }
