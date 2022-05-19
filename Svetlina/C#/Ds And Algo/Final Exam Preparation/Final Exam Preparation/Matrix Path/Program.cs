515dff84355b916a43b036b8b36c8ef2cb1111a5
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-05-19 12:41:15 +0300

    Initial commit

---
+++ Svetlina/C#/Ds And Algo/Final Exam Preparation/Final Exam Preparation/Matrix Path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Matrix_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
