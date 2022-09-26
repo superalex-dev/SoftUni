07e76d380affe075b47ea3cda3df348c3e3166e1
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-09-26 10:16:41 +0300

    Initial commit

---
+++ Svetlina/11th grade/Entrance Exam 2022 prep/entrance exam prep/entrance exam prep/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace entrance_exam_prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Graph graph = new Graph(n, m);
+            graph.ReadGraph();
+
+            Console.WriteLine(graph.IsCyclic());
+        }
+    }
+}
