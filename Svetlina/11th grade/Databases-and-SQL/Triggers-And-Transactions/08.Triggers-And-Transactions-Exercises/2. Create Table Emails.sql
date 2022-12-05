a0573f07e46cfe7a45025fe6b6feef4d8785c457
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-12-05 09:28:39 +0200

    Initial commit

--- Svetlina/11th grade/Databases-and-SQL/Triggers-And-Transactions/08.Triggers-And-Transactions-Exercises/2. Create Table Emails.sql
+++ Svetlina/11th grade/Databases-and-SQL/Triggers-And-Transactions/08.Triggers-And-Transactions-Exercises/2. Create Table Emails.sql
@@ -3,25 +3,11 @@ FOR INSERT
 AS
      BEGIN
          INSERT INTO NotificationEmails
-         VALUES
-         (
-         (
-             SELECT AccountId
-             FROM inserted
-         ),
-         CONCAT('Balance change for account: ',
-               (
-                   SELECT AccountId
-                   FROM inserted
-               )),
-         CONCAT('On ', FORMAT(GETDATE(), 'dd-MM-yyyy HH:mm'), ' your balance was changed from ',
-               (
-                   SELECT OldSum
-                   FROM Logs
-               ), ' to ',
-               (
-                   SELECT NewSum
-                   FROM Logs
-               ), '.')
-         );
-     END;
+         VALUES((SELECT AccountId
+		 FROM inserted),
+         CONCAT('Balance change for account: ', (SELECT AccountId
+		 FROM inserted)),
+         CONCAT('On ', FORMAT(GETDATE(), 'dd-MM-yyyy HH:mm'), ' your balance was changed from ', (SELECT OldSum
+		 FROM Logs), ' to ', (SELECT NewSum
+		 FROM Logs), '.'));
+     END
