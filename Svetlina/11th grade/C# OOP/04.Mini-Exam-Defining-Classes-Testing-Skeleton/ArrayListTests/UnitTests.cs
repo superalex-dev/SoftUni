abfffcd5e57b847e5add7de3ee8ae669b21e300f
Alexander Boev <mr.naidobrixjr@gmail.com>
2023-02-20 10:17:24 +0200

    Initial commit

---
+++ Svetlina/11th grade/C# OOP/04.Mini-Exam-Defining-Classes-Testing-Skeleton/ArrayListTests/UnitTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using System;
+using UnitTesting;
+
+namespace ArrayListTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_ArrayList_Constructor()
+        {
+
+        }
+
+    }
+}
