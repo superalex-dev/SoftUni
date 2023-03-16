f63088d85257691f5b8f2ac3b1ddda541c80e8bc
Alexander Boev <mr.naidobrixjr@gmail.com>
2023-03-16 09:30:38 +0200

    Initial commit

--- Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,27 @@
+const baseUrl = 'http://localhost:3030/jsonstore/bus/businfo/';
+
 function getInfo() {
-    console.log("TODO...");
+  const stopId = document.getElementById('stopId').value;
+  const stopNameEl = document.getElementById('stopName');
+  const busesListEl = document.getElementById('buses');
+
+  stopNameEl.textContent = '';
+  busesListEl.innerHTML = '';
+
+  fetch(baseUrl + stopId)
+    .then(response => response.json())
+    .then(data => {
+      stopNameEl.textContent = data.name;
+
+      Object.entries(data.buses).forEach(([busId, time]) => {
+        const li = document.createElement('li');
+        li.textContent = `Bus ${busId} arrives in ${time}`;
+        busesListEl.appendChild(li);
+      });
+    })
+    .catch(() => {
+      stopNameEl.textContent = 'Error';
+    });
 }
+
+document.getElementById('submit').addEventListener('click', getInfo);
