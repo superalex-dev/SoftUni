e9691d3f522a76b039faa5bc69944c162d95ad9f
Alexander Boev <mr.naidobrixjr@gmail.com>
2023-03-16 09:33:37 +0200

    Initial commit

--- Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,27 +1,27 @@
-const baseUrl = 'http://localhost:3030/jsonstore/bus/businfo/';
+const stopNameElement = document.getElementById('stopName');
+const busesListElement = document.getElementById('buses');
 
-function getInfo() {
+document.getElementById('submit').addEventListener('click', loadBusInfo);
+
+function loadBusInfo() {
   const stopId = document.getElementById('stopId').value;
-  const stopNameEl = document.getElementById('stopName');
-  const busesListEl = document.getElementById('buses');
-
-  stopNameEl.textContent = '';
-  busesListEl.innerHTML = '';
-
-  fetch(baseUrl + stopId)
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+  
+  busesListElement.innerHTML = '';
+  
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-      stopNameEl.textContent = data.name;
-
-      Object.entries(data.buses).forEach(([busId, time]) => {
-        const li = document.createElement('li');
-        li.textContent = `Bus ${busId} arrives in ${time}`;
-        busesListEl.appendChild(li);
-      });
+      const { name, buses } = data[stopId];
+      stopNameElement.textContent = name;
+      
+      for (const [busId, time] of Object.entries(buses)) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Bus ${busId} arrives in ${time} minutes`;
+        busesListElement.appendChild(listItem);
+      }
     })
     .catch(() => {
-      stopNameEl.textContent = 'Error';
+      stopNameElement.textContent = 'Error';
     });
 }
-
-document.getElementById('submit').addEventListener('click', getInfo);
