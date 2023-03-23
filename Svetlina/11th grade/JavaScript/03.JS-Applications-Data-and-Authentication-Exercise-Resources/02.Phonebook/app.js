97cc5a2bea99a999b48863bdc810850dcd79051f
Alexander Boev <mr.naidobrixjr@gmail.com>
2023-03-23 10:23:00 +0200

    Initial commit

--- Svetlina/11th grade/JavaScript/03.JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ Svetlina/11th grade/JavaScript/03.JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,72 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+async function attachEvents() {
+    const phonebook = document.getElementById('phonebook');
+    const btnLoad = document.getElementById('btnLoad');
+    const personName = document.getElementById('person');
+    const personPhone = document.getElementById('phone');
+    const btnCreate = document.getElementById('btnCreate');
 
+    btnLoad.addEventListener('click', loadContacts);
+    btnCreate.addEventListener('click', createContact);
+
+    async function loadContacts() {
+        phonebook.innerHTML = '';
+        try {
+            const response = await fetch('http://localhost:3030/jsonstore/phonebook');
+            if (!response.ok) {
+                throw new Error('Unable to fetch contacts.');
+            }
+            const contacts = await response.json();
+            for (const [id, contact] of Object.entries(contacts)) {
+                const li = document.createElement('li');
+                li.textContent = `${contact.person}: ${contact.phone}`;
+
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent = 'Delete';
+
+                deleteBtn.addEventListener('click', async () => {
+                    try {
+                        const response = await fetch(`http://localhost:3030/jsonstore/phonebook/${id}`, {
+                            method: 'DELETE'
+                        });
+                        if (!response.ok) {
+                            throw new Error('Unable to delete contact.');
+                        }
+                        loadContacts();
+                    } catch (err) {
+                        displayError(err);
+                    }
+                });
+
+                li.append(deleteBtn);
+                phonebook.append(li);
+            }
+        } catch (err) {
+            displayError(err);
+        }
+
+        async function createContact() {
+            try {
+                const response = await fetch('http://localhost:3030/jsonstore/phonebook', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({person: personName.value, phone: personPhone.value})
+                });
+                if (!response.ok) {
+                    throw new Error('Unable to create contact.');
+                }
+                personName.value = '';
+                personPhone.value = '';
+                loadContacts();
+            } catch (err) {
+                displayError(err);
+            }
+        }
+
+        function displayError(err) {
+            const li = document.createElement('li');
+            li.textContent = 'Error: ' + err;
+            phonebook.append(li);
+        }
+    }
+}
 attachEvents();
