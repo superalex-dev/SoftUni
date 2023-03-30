96dfcc532f0e5b8458531ea5bf4bc39dad67960a
Alexander Boev <mr.naidobrixjr@gmail.com>
2023-03-30 08:26:45 +0300

    Initial commit

--- Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ Svetlina/11th grade/JavaScript/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,55 @@
 function attachEvents() {
-    console.log('TODO...');
+    const buttonPosts = document.getElementById('btnLoadPosts');
+    const buttonComments = document.getElementById('btnViewPost');
+    const select = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-comments');
+
+    buttonPosts.addEventListener('click', loadPosts);
+    buttonComments.addEventListener('click', displayPost);
+
+    async function loadPosts() {
+        try {
+            const response = await fetch('http://localhost:3030/jsonstore/blog/posts');
+            const data = await response.json();
+
+            Object.values(data).forEach((post) => {
+                const option = document.createElement('option');
+                option.value = post.id;
+                option.textContent = post.title;
+                select.appendChild(option);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function displayPost() {
+        const postId = select.value;
+
+        try {
+            const commentsResponse = await fetch('http://localhost:3030/jsonstore/blog/comments');
+            const commentsData = await commentsResponse.json();
+            const comments = Object.values(commentsData).filter((comment) => comment.postId == postId);
+
+            postComments.innerHTML = '';
+
+            comments.forEach((comment) => {
+                const li = document.createElement('li');
+                li.textContent = comment.text;
+                postComments.appendChild(li);
+            });
+
+            const postResponse = await fetch(`http://localhost:3030/jsonstore/blog/posts/${postId}`);
+            const postData = await postResponse.json();
+
+            postTitle.textContent = postData.title;
+            postBody.textContent = postData.body;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 attachEvents();
