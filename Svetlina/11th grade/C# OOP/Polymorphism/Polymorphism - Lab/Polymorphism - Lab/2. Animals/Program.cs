7e54e04b5174214fe45dcefa351c650f980bbfaf
Alexander Boev <naidobrixxjr@gmail.com>
2023-04-24 09:55:21 +0300

    Initial commit

---
+++ Svetlina/11th grade/C# OOP/Polymorphism/Polymorphism - Lab/Polymorphism - Lab/2. Animals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Svetlina/11th grade/C# OOP/Polymorphism/Polymorphism - Lab/Polymorphism - Lab/Polymorphism - Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using Polymorphism___Lab;
+using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOperation = new MathOperation();
+            Console.WriteLine(mathOperation.Add(2, 3));
+            Console.WriteLine(mathOperation.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperation.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
