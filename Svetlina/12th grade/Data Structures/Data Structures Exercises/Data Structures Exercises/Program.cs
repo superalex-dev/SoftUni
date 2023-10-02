8451bd2bd157c0979be98fa92671abf60fc2ddd1
superalex-dev <mr.naidobrixjr@gmail.com>
2023-10-02 20:32:22 +0300

    1. Message

---
+++ Svetlina/12th grade/Data Structures/Data Structures Exercises/Data Structures Exercises/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<char> message = new Stack<char>();
+        string input = Console.ReadLine();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '*')
+            {
+                if (message.Count > 0)
+                    message.Pop();
+            }
+            else
+            {
+                message.Push(input[i]);
+            }
+        }
+
+        int messageLength = message.Count;
+
+        for (int i = 0; i < messageLength; i++)
+        {
+            Console.Write(message.Pop());
+        }
+    }
+}
