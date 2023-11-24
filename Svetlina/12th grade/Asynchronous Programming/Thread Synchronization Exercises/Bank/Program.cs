6935cb362990d5f1452a8b0239f521574d8599c3
Alexander Boev <naidobrixxjr@gmail.com>
2023-11-24 09:46:42 +0200

    ./.

---
+++ Svetlina/12th grade/Asynchronous Programming/Thread Synchronization Exercises/Bank/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+class Account
+{
+    private int balance;
+    private readonly object balanceLock = new object();
+
+    public Account(int initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public int Withdraw(int amount)
+    {
+        lock (balanceLock)
+        {
+            if (balance < 0)
+            {
+                throw new Exception("Negative Balance");
+            }
+
+            if (balance >= amount)
+            {
+                Console.WriteLine($"Balance before Withdrawal: {balance}");
+                Console.WriteLine($"Amount to Withdraw: -{amount}");
+                balance -= amount;
+                Console.WriteLine($"Balance after Withdrawal: {balance}");
+                return amount;
+            }
+
+            return 0;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Account account = new Account(500);
+        Thread[] threads = new Thread[10];
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(() => {
+                try
+                {
+                    account.Withdraw(100);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+}
