d4375829e5c5e05d1bc3b2cbb537dbe66ddaf3f2
Alexander Boev <naidobrixxjr@gmail.com>
2023-11-24 09:39:22 +0200

    ./.

---
+++ Svetlina/12th grade/Asynchronous Programming/Thread Synchronization Exercises/Reserve Seat on a Train/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reserve_Seat_on_a_Train
+{
+    public class Program
+    {
+        private static bool reserve = false;
+        private static readonly object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Reserve);
+            Reserve();
+        }
+
+        static void Reserve()
+        {
+            lock (lockObject)
+            {
+                if (!reserve)
+                {
+                    Console.WriteLine("Reserved");
+                    reserve = true;
+                }
+            }
+        }
+    }
+}
---
+++ Svetlina/12th grade/C#/DataStructures/Combinatorics Exercise/Combinatorics exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public int FindSecondMinimumValue(TreeNode root) {
+    int minVal = root.val;
+    int? secondMinVal = null;
+    
+    if (root == null || root.left == null && root.right == null) 
+    {
+        return -1;
+    }
+
+    DFS(root, ref minVal, ref secondMinVal);
+}
+
+public void DFS(TreeNode node, ref int minVal, ref int? secondMinVal) {
+    if (node == null) {
+        return;
+    }
+    
+    if (node.val > minVal && (!secondMinVal.HasValue || node.val < secondMinVal)) {
+        secondMinVal = node.val;
+    }
+
+    DFS(node.left, ref minVal, ref secondMinVal);
+    DFS(node.right, ref minVal, ref secondMinVal);
+}
